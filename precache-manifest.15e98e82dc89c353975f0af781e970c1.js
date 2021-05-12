1c87749870abb423084c36b89000585bf62f2c73
Lorena <lorena.martins.1600@gmail.com>
2021-05-11 21:24:43 -0300

    Initial commit

---
+++ precache-manifest.15e98e82dc89c353975f0af781e970c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae33fd2d0be094f1416928d09458907",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cdfca805d5f22111a3e8",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5c56d3e370bb3c1d017f",
+    "url": "./static/css/main.e36eed5e.chunk.css"
+  },
+  {
+    "revision": "cdfca805d5f22111a3e8",
+    "url": "./static/js/2.a1601f6f.chunk.js"
+  },
+  {
+    "revision": "ad22816ffba46753536aeac2978d14e8",
+    "url": "./static/js/2.a1601f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c56d3e370bb3c1d017f",
+    "url": "./static/js/main.215974a8.chunk.js"
+  },
+  {
+    "revision": "886ef8a76d39f7d0ef15",
+    "url": "./static/js/runtime-main.1b861e22.js"
+  }
+]);
